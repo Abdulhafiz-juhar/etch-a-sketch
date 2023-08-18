90b334ce90d159fcd6d777786b86fe976192f06f
Abdulhafiz-juhar <abdulhafizjuharajmbest@gmail.com>
2023-08-18 15:05:15 +0300

    Add color change effect when hover in boxes

--- script.js
+++ script.js
@@ -7,9 +7,17 @@ for (let j = 0; j < col; j++) {
 
     for (let i = 0; i < row; i++) {
         let box = document.createElement('div');
+        box.classList.add('boxes');
         box.style.cssText = 'width: 5em; height: 5em; background-color: blue; border: solid';
         column.appendChild(box);
     }
 
     container.appendChild(column);
 }
+
+function mark() {
+    this.style.backgroundColor = 'red';
+}
+
+const boxes = document.querySelectorAll('.boxes');
+boxes.forEach(box => box.addEventListener('mouseenter', mark));
