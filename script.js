df69460267f674380f482517d81569b325779a6b
Abdulhafiz-juhar <abdulhafizjuharajmbest@gmail.com>
2023-08-18 17:08:17 +0300

    Remove commented out older box displaying loop method

--- script.js
+++ script.js
@@ -5,22 +5,6 @@ let row = 16;
 let totalBoxes=row*row;
 let basisPercent = 100/row;
 
-
-// for (let j = 0; j < col; j++) {
-//     const column = document.createElement('div');
-
-//     for (let i = 0; i < row; i++) {
-//         let box = document.createElement('div');
-//         box.classList.add('boxes');
-//         box.style.cssText = 'flex: 1; background-color: blue; border: solid';
-//         column.appendChild(box);
-//     }
-
-//     container.appendChild(column);
-// }
-
-
-//adjust the percent of flex-basis from 20% to be dynamic
     for (let i = 0; i < totalBoxes; i++) {
         let box = document.createElement('div');
         box.classList.add('boxes');
