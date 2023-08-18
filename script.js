799c7e9d32501ed1dabe7a346c7d4dd2df379594
Abdulhafiz-juhar <abdulhafizjuharajmbest@gmail.com>
2023-08-18 11:50:56 +0300

    Create div boxes based on row and column

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
- 
+const container = document.querySelector('.container'); 
+container.style.cssText = 'display: flex';
+let row=5; col=5;
+
+for (let j = 0; j < col; j++) {
+    const column = document.createElement('div');
+
+    for (let i = 0; i < row; i++) {
+        let box = document.createElement('div');
+        box.style.cssText = 'width: 5em; height: 5em; background-color: blue; border: solid';
+        column.appendChild(box);
+    }
+
+    container.appendChild(column);
+}
