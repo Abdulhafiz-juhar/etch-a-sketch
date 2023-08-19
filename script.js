6f5602b62033560d91955df24a33b117314cad10
Abdulhafiz-juhar <abdulhafizjuharajmbest@gmail.com>
2023-08-19 06:35:43 +0300

    Rearrange loops into function to make the size button fire itwhen clicked

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 const container = document.querySelector('.container'); 
 container.style.cssText = 'display: flex';
 
-const sizeButton = document.querySelector('.size');
+let row =16;
+function createBoxes () {
+    let totalBoxes=row*row;
+    let basisPercent = 100/row;
+    
+        for (let i = 0; i < totalBoxes; i++) {
+            let box = document.createElement('div');
+            box.classList.add('boxes');
+            box.style.cssText = `flex: 0 0 ${basisPercent}%; background-color: blue; border: solid`;
+            container.appendChild(box);
+        }
 
-
-let row = 16;
-let totalBoxes=row*row;
-let basisPercent = 100/row;
-
-    for (let i = 0; i < totalBoxes; i++) {
-        let box = document.createElement('div');
-        box.classList.add('boxes');
-        box.style.cssText = `flex: 0 0 ${basisPercent}%; background-color: blue; border: solid`;
-        container.appendChild(box);
-    }
+        const boxes = document.querySelectorAll('.boxes');
+        boxes.forEach(box => box.addEventListener('mouseenter', mark));
+}
 
 function mark() {
     this.style.backgroundColor = 'red';
 }
 
-const boxes = document.querySelectorAll('.boxes');
-boxes.forEach(box => box.addEventListener('mouseenter', mark));
+createBoxes();
+function enterSize() {
+   row = parseInt(prompt('Please enter the size'));
+   container.textContent = '';
+   createBoxes();
+}
+
+const sizeButton = document.querySelector('.size');
+sizeButton.addEventListener('click', enterSize);
